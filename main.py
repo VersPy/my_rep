a1ff4cb0f782a7c697d268ab80ea60d1e214cfae
VersPy <83089973+VersPy@users.noreply.github.com>
2021-11-22 19:40:18 +0300

    commit 2

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import sys
 
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+# Наследуемся от виджета из PyQt5.QtWidgets и от класса с интерфейсом
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+        self.wedht = self.centralWidget().width()
+        self.height = self.centralWidget().height()
+
+    def paint(self):
+        import random
+        self.wedht = random.randint(1, self.centralWidget().width())
+        self.height = random.randint(1, self.centralWidget().height())
+        self.r = random.randint(1, 100)
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(self.wedht, self.height, self.r, self.r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
